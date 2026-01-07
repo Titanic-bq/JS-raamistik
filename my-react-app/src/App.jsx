be5d491d1356d0129cd07f29a2a2d2f09e4d8644
hendrik <hendrik.jurgenson@voco.ee>
2026-01-07 11:11:37 +0200

    3. Komponendi sisustamine andmetega, andmete rakendamine

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,11 +2,24 @@ import ExpenseItem from "./components/ExpenseItem.jsx";
 import "./App.css"
 
 const App = () => {
-    return (
+    const expenses =[
+        {
+            date: new Date(2024, 10, 12),
+            title: "New book" ,
+            price: 30.99
+        },
+         {
+            date: new Date(2024, 10, 12),
+            title: "New Jeans" ,
+            price: 99.99
+        }
+    ]
+return (
         <div className="App">
-            <ExpenseItem />
+            <ExpenseItem data={expenses[0]}/>
+              <ExpenseItem data={expenses[1]}/>
         </div>
     );
 }
 
-export default App
+export default App;
