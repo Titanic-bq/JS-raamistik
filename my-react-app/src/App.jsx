eea77fae73bf66dbfa5116b53cd3fcae7b1da7fa
hendrik <hendrik.jurgenson@voco.ee>
2026-01-07 10:00:16 +0200

    2. Uue komponendi loomine ja kujundamine Lõpptulemus: tehtud, õppisin, et failid peaved olema jsx, mitte js.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem.jsx";
+import "./App.css"
 
-
-
-
+const App = () => {
+    return (
+        <div className="App">
+            <ExpenseItem />
+        </div>
+    );
+}
 
 export default App
