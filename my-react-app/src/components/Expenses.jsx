06e0df4e48886f4ab9c3161c167ef2a899e99e25
hendrik <hendrik.jurgenson@voco.ee>
2026-02-04 20:34:04 +0200

    Ülesanne 4 - sisekomponendi andmete suunamine välikomponendile

--- my-react-app/src/components/Expenses.jsx
+++ my-react-app/src/components/Expenses.jsx
@@ -1,12 +1,30 @@
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'       
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
+
+      const [filteredYear, setFilteredYear] = useState('2025');
+  console.log('Expenses: hetkel valitud aasta', filteredYear);
+
+   const filterChangeHandler = (selectedYear) => {
+    console.log('Expenses: sain ExpensesFilter-ist', selectedYear);
+    setFilteredYear(selectedYear);
+  };
     return (
         <Card className="expenses">
+            <ExpensesFilter
+            selected={filteredYear}
+            onFilterChange={filterChangeHandler} />
+
+
             {props.items.map(expense => (
-                <ExpenseItem key={expense.date} expenseData={expense} />
+
+          <ExpenseItem
+           key={expense.date} 
+           expenseData={expense} 
+           />
             ))}
         </Card>
     )
