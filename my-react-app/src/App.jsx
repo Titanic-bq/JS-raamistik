a371d19266c7c43858c15f07cd139bbd3294bf99
hendrik <hendrik.jurgenson@voco.ee>
2026-02-04 21:09:47 +0200

    14. Ülesanne 5 - dünaamilise kuvamise täiendamine

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     const DYMMY_EXPENSES =[
         {
             id:"id1",
-            date: new Date(2024, 10, 12),
+            date: new Date(2023, 10, 12),
             title: "New book" ,
             price: 30.99
         },
--- my-react-app/src/components/Expenses.jsx
+++ my-react-app/src/components/Expenses.jsx
@@ -4,43 +4,38 @@ import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
-    const filterYearHandler= (selectedYear) => {
-        console.log("Year data in Expenses.jsx" + selectedYear);
-    };
+    const [filteredYear, setFilteredYear] = useState('2025');
 
-
-
-    props.expenses.map((expense) => {
-       console.log(expense)
-    });
-};
-
-    
-
-
-
-
-function Expenses(props) {
-
-      const [filteredYear, setFilteredYear] = useState('2025');
   console.log('Expenses: hetkel valitud aasta', filteredYear);
+  console.log('Expenses: kõik kulud', props.items);
 
-   const filterChangeHandler = (selectedYear) => {
+  const filterChangeHandler = (selectedYear) => {
     console.log('Expenses: sain ExpensesFilter-ist', selectedYear);
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  console.log('Expenses: filtreeritud kulud', filteredExpenses);
+
+  
     return (
         <Card className="expenses">
             <ExpensesFilter
+                selected={filteredYear}
             onChangeFilter={filterChangeHandler} />
+  {filteredExpenses.length === 0 && (
+        <p>Kulusid ei leitud</p>
+      )}
 
-
-            {props.items.map(expense => {
-                return <ExpenseItem expenseData={expense}
-                key={expense.id}
-            />;
-            })
-            }
+      {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          expenseData={expense}
+        />
+      ))}
         </Card>
     )
 }
