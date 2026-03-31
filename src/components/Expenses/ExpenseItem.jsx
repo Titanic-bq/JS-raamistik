f100220fb48b53657fee95b12003dc7182146a58
hendrik <hendrik>
2026-03-31 17:44:38 +0300

    lähen commit Feb 12, 2026 React.domcreate portal index.html overlays

---
+++ src/components/Expenses/ExpenseItem.jsx
@@ -0,0 +1,31 @@
+import './ExpenseItem.css'
+import Card from '../../UI/Card.jsx'
+import ExpenseDate from "./ExpenseDate.jsx";
+import { useState } from 'react';
+
+const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.expenseData.title)
+    const clickHandler = () => {
+        console.log('Clicked!')
+        setTitle('Updated by click ${title}')
+        console.log(title)
+    }
+
+    return (
+        <li>
+        <Card className="expense-item">
+            <ExpenseDate date={props.expenseData.date}></ExpenseDate>
+            <div className="expense-item__description">
+                <h2>{props.expenseData.title}</h2>
+                <div className="expense-item__price">{props.expenseData.price}</div>
+            </div>
+            <button onClick={clickHandler}>Click Me</button>
+        </Card>
+        </li>
+    )
+}
+
+export default ExpenseItem
+
+
+
